2fda26f24ba437b857d64ef016fd647d07e7d2ad
du201 <duslg0114@gmail.com>
2021-05-08 12:55:36 -0400

    research data collection

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 const AboutPage = () => {
   return (
     <div className="w-100 d-flex align-items-center justify-content-center" style={{height:"92vh", fontSize: "1rem"}}>
-      <p className="text-center">
+      <p className="text-center" style={{fontSize: '2rem'}}>
         Welcome to the website for the study <br />
         "A Survey on Machine Learning-based Software Testing Literature: <br />
             Checking the Validity of Proposed Classification Systems"  <br />
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,9 +1,37 @@
 import React from "react";
 import { useState, useEffect } from 'react';
+import { Card } from 'antd';
+import bolu from '../images/bolu.jpg';
+import shiqi from '../images/shiqi.jpg';
+import xin from '../images/xin.jpg';
+
+const { Meta } = Card;
 
 const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+    <div className="d-flex flex-row justify-content-around vh-100 align-items-center">
+      <Card
+        hoverable
+        style={{ width: 240 }}
+        cover={<img alt="example" src={bolu} />}
+      >
+        <Meta title="Boluwatife Fashina" description="Purdue ECE Graduate Student" />
+      </Card>
+      <Card
+        hoverable
+        style={{ width: 240 }}
+        cover={<img alt="example" src={shiqi} />}
+      >
+        <Meta title="Shiqi Wang" description="Purdue ECE Undergraduate Student" />
+      </Card>
+      <Card
+        hoverable
+        style={{ width: 240 }}
+        cover={<img alt="example" src={xin} />}
+      >
+        <Meta title="Xin Du" description="Purdue ECE Undergraduate Student" />
+      </Card>
+    </div>
   );
 };
 
